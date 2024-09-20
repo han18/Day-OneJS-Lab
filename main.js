7a34a3ce2316b2a1e58139a5de3204e5630a783f
han18 <han.fadah@gmail.com>
2024-09-20 10:26:56 -0400

    working on odd or even numbers

---
+++ main.js
@@ -0,0 +1,19 @@
+// 
+
+// const myName = "Hanan";
+
+// console.log(myName)
+
+
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+const isSum50 = (n1 + n2 + n3 + n4);
+
+console.log(isSum50);
+
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 3;
+
+console.log(isTwoOdd);
